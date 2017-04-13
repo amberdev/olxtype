cfdcd7348175813f36e2cbf479e93ced4e9a33ed
dipanshu1989 <dipanshu.swiss.dreams@gmail.com>
2017-04-13 23:51:27 +0530

    changes from local

--- application/models/Usersapi.php
+++ application/models/Usersapi.php
@@ -117,11 +117,24 @@ class Usersapi extends CI_Model
 
 	function all_products()
 	{
-		$this->db->where('status','t');
-		$q=$this->db->get('tbl_products');
+		$this->db->select('tbl_products.*,tbl_image.*');
+		$this->db->from('tbl_products');
+		$this->db->join('tbl_image','tbl_image.product_id=tbl_products.id');
+		$this->db->where('tbl_products.status','t');
+		$q=$this->db->get();
+		echo $this->db->last_query();die;
 		if($q->num_rows()>0)
 		{
 			return $q->result_array();
+		}
+	}
+
+	function save_image($image,$product_id)
+	{
+		if($product_id!='')
+		{
+			$data=array('product_id'=>$product_id,'image'=>$image);
+			$this->db->insert('tbl_image',$data);
 		}
 	}
 
